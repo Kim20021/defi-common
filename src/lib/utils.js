f2a9e2130b68cb19492b5f217862fa53293904b6
xulei <gray.xu@tron.network>
2021-07-02 10:35:43 +0800

    feat: add notification & util

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -240,55 +240,6 @@ export const numberParser = (str, decimal) => {
   }
 };
 
-export const openTransModal = (intlObj = {}, { step = 0, txId = '' }) => {
-  modalRef && modalRef.destroy();
-
-  if (!step) return;
-
-  const config = {
-    title: '',
-    className: 'trans-modal',
-    icon: null,
-    // width: 630,
-    // style: { marginLeft: getModalLeft() },
-    content: (
-      <div className="trans-modal-body center">
-        <div className="trans-modal-title">{intl.get(intlObj.title, intlObj.obj)}</div>
-        {step == 1 ? (
-          <React.Fragment>
-            <div className="trans-modal-icon">
-              <LoadingOutlined style={{ fontSize: '80px' }}></LoadingOutlined>
-            </div>
-            <div className="trans-modal-status trans-modal-wait-confirm">{intl.get('deposit.explanation2')}</div>
-            {/* <div className="trans-modal-tips trans-modal-wait-confirm-tips">{intl.get('deposit.explanation2')}</div> */}
-          </React.Fragment>
-        ) : null}
-        {step == 2 ? (
-          <React.Fragment>
-            <div className="trans-modal-icon">
-              <img src={TransSubmittedIcon} alt="" style={{ width: '86px' }} />
-            </div>
-            {/* <div className="trans-modal-status trans-modal-submit">{intl.get('deposit.explanation4')}</div> */}
-            <div className="trans-modal-tips trans-modal-submit-tips">
-              {tronscanTX(intl.get('deposit.explanation4'), txId)}
-            </div>
-          </React.Fragment>
-        ) : null}
-        {step == 3 ? (
-          <React.Fragment>
-            <div className="trans-modal-icon">
-              <img src={TransCancelledIcon} alt="" style={{ width: '86px' }} />
-            </div>
-            <div className="trans-modal-status trans-modal-cancel">{intl.get('deposit.explanation3')}</div>
-          </React.Fragment>
-        ) : null}
-      </div>
-    )
-  };
-
-  modalRef = Modal.info(config);
-};
-
 export const setTransactionsData = (tx, intlObj) => {
   let data = window.localStorage.getItem(window.defaultAccount) || '[]';
   let dataArr = JSON.parse(data);
